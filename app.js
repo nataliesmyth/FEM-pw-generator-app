40166c210bec0f47051f581ac7ed1b9ec2ed8d91
Nataliesmyth <nataliesmyth11@gmail.com>
2024-06-07 17:25:00 -0400

    mobile styling

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log('sanity check');
+
+const checkbox = document.querySelectorAll('.checkbox');
+const checkImg = document.querySelectorAll('.check-image')
+
+function handleCheckboxClick() {
+    console.log('click!')
+}
+
+checkbox.forEach((check) => {
+    
+    check.addEventListener('click', () => {
+        console.log(check)
+        // check.classList.toggle('.active');
+        // checkImg.classList.toggle('.hidden');
+        console.log(check.style.backgroundColor)
+        if (check.style.backgroundColor === 'hsl(127 100 82)' ||
+            check.style.backgroundColor === 'rgb(163, 255, 174)') {
+                check.style.backgroundColor = 'hsl(248 15 11)';
+                check.style.borderColor = '#FFFFFF'
+            } else {
+                check.style.backgroundColor = 'hsl(127 100 82)';
+                check.style.borderColor = 'transparent';
+        }
+        checkImg.style.display = 'block';
+    });
+});
+

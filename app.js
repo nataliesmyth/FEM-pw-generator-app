a2cb80ede0546fad16c6362760f6b0f05cee8fa0
Nataliesmyth <nataliesmyth11@gmail.com>
2024-06-09 17:55:04 -0400

    refectored js using classlist toggle

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const generatedPw = document.getElementById('passwordOutput').innerHTML;
 console.log(generatedPw);
 const checkbox = document.querySelectorAll('.checkbox');
 const checkImg = document.querySelectorAll('.check-image');
+let newPassword = ''
 
 let charLength = 0;
 
@@ -34,31 +35,13 @@ checkbox.forEach((check) => {
     check.addEventListener('click', () => {
         console.log(check)
         console.log(check.id)
-        // check.classList.toggle('.active');
-        // checkImg.classList.toggle('.hidden');
-        console.log(check.style.backgroundColor)
-        if (check.style.backgroundColor === 'hsl(127 100 82)' ||
-            check.style.backgroundColor === 'rgb(163, 255, 174)') {
-                check.style.backgroundColor = 'hsl(248 15 11)';
-                check.style.borderColor = '#FFFFFF'
-            } else {
-                check.style.backgroundColor = 'hsl(127 100 82)';
-                check.style.borderColor = 'transparent';
-        }
-        // checkImg.style.display = 'block';
+        check.classList.toggle('active');
+        console.log(check.classList);
     });
 });
 
-// window.addEventListener('load', () => {
-//     charLengthNum.innerText = charLength;
+function handleGenerateBtnClick() {
+    console.log('clicked!');
+};
 
-//     console.log('loaded!');
-//     console.log(checkbox)
-//     checkbox.forEach((e) => {
-//         if (e.id !== 'includeSymbols') {
-//             console.log(e.id)
-//             e.style.backgroundColor = 'hsl(127 100 82)';
-//             e.style.borderColor = 'transparent';
-//         }
-//     });
-// });
+generateBtn.addEventListener('click', handleGenerateBtnClick);

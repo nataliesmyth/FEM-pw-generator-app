f7e46a6617971d7f04eb470cbe1800baed6da1fe
Nataliesmyth <nataliesmyth11@gmail.com>
2024-06-11 16:49:26 -0400

    added strength graph colors for each strength level

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ charOutput.innerText = slider.value;
 slider.oninput = function() {
     charOutput.innerText = this.value;
 }
+console.log(slider.value)
 // let charLength = 0;
 // let sliderLeft = document.getElementById('sliderLeft');
 // if (charLength > 0) {
@@ -23,6 +24,10 @@ let lowerCaseActive = false;
 let numbersActive = false;
 let symbolsActive = false;
 
+// strength state
+const strengthLevel = document.getElementById('strengthResult');
+const graph = document.querySelector('.strength-result-graph');
+console.log(graph.firstChild)
 
 const copyContent = async () => {
     try {
@@ -80,25 +85,76 @@ checkbox.forEach((check) => {
     console.log(check.classList)
 });
 
-function isActive() {
+const Required = {
+    upppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    numbers: '1234567890',
+    symbols: '!@#$%^&*'
+}
+function generatePassword(length, required) {
 
+}
+
+let reqNum = 0;
+function setStrengthState(length) {
+    length = slider.value;
+    if (length < 6) {
+        console.log('very weak!')
+        strengthLevel.innerText = 'very weak!'
+        graph.firstElementChild.style.backgroundColor = 'hsl(var(--clr-red))';
+        graph.firstElementChild.style.border = 'none';
+    } else if (length >=6 && length < 10) {
+        strengthLevel.innerText = 'weak '
+        graph.firstElementChild.style.backgroundColor = 'hsl(var(--clr-orange))';
+        graph.firstElementChild.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-orange))';
+        graph.firstElementChild.nextElementSibling.style.border = 'none';
+    } else if (length >= 10 && length < 15) {
+        strengthLevel.innerText = 'medium';
+        graph.firstElementChild.style.backgroundColor = 'hsl(var(--clr-yellow))';
+        graph.firstElementChild.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-yellow))';
+        graph.firstElementChild.nextElementSibling.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-yellow))';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.style.border = 'none';
+    } else {
+        strengthLevel.innerText = 'strong'
+        graph.firstElementChild.style.backgroundColor = 'hsl(var(--clr-accent))';
+        graph.firstElementChild.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-accent))';
+        graph.firstElementChild.nextElementSibling.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-accent))';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.style.border = 'none';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.style.backgroundColor = 'hsl(var(--clr-accent))';
+        graph.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.style.border = 'none';
+    }
 }
 
 function handleGenerateBtnClick() {
     copyText.classList.add('hidden')
+    console.log('this password needs to be ' + slider.value + ' characters long')
+    
     if (upperCaseActive === true) {
+        reqNum += 1;
         console.log('uppercase letters required!');
     }
     if (lowerCaseActive === true) {
+        reqNum += 1;
         console.log('lowercase letters required!');
     }
     if (numbersActive === true) {
+        reqNum += 1;
         console.log('numbers required!');
     }
     if (symbolsActive === true) {
+        reqNum += 1;
         console.log('symbols required!');
     }
-
+    if (slider.value >= 6 && slider.value < 15 && reqNum > 1) {
+        console.log('password strength: medium')
+    }
+    console.log(reqNum)
+    setStrengthState(slider.value);
 };
 
 generateBtn.addEventListener('click', handleGenerateBtnClick);
